450415c2e7a1329b8d51ec1107d1a273f3c1371c
sam <semkamirov@gmail.com>
2023-12-06 10:52:59 +0500

    Настраивает webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,19 @@ module.exports = {
         }]
       }
     )
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      }
+    ]
+  }
 }
