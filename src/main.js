54dd80d85659c802491f195e6116f38adbeeca83
sam <semkamirov@gmail.com>
2023-12-07 18:32:41 +0500

    Отрисовывает компоненты

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-
-import BoardPresenter from "./presenter/board-presenter";
-import {render} from "./render";
-import {ListFilterView} from "./view/filter/list-filter-view";
+import BoardPresenter from './presenter/board-presenter.js';
+import {render} from './render.js';
+import {ListFilterView} from './view/filter/list-filter-view.js';
 
 const mainContainer = document.querySelector('.trip-events');
 const headerContainer = document.querySelector('.trip-main');
