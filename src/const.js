8ccb6c992888018d7159455a7ad25e5b4a7eedbb
sam <semkamirov@gmail.com>
2024-02-07 11:01:33 +0500

    Обновляет модель. Добавляет фильтрацию

---
+++ src/const.js
@@ -0,0 +1,105 @@
+const TRIP_TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+
+const BASE_URL = 'https://22.objects.htmlacademy.pro/spec/big-trip';
+
+const AUTHORIZATION_STRING = 'Basic sfjdlkjdfhg';
+
+const FilterType = {
+  EVERYTHING: 'EVERYTHING',
+  FUTURE: 'FUTURE',
+  PRESENT: 'PRESENT',
+  PAST: 'PAST'
+};
+
+const SortingType = {
+  DAY: {name: 'day', isDisabled: false},
+  EVENT: {name: 'event', isDisabled: true},
+  TIME: {name: 'time', isDisabled: false},
+  PRICE: {name: 'price', isDisabled: false},
+  OFFER: {name: 'offer', isDisabled: true}
+};
+
+const BLANK_POINT = {
+  'type': TRIP_TYPES[5].toLowerCase(),
+  'destination': '',
+  'dateFrom': null,
+  'dateTo': null,
+  'basePrice': 0,
+  'offers': [],
+  'isFavorite': false
+};
+
+const EmptyListMessage = {
+  EVERYTHING: 'Click New Event to create your first point',
+  PAST: 'There are no past events now',
+  PRESENT: 'There are no present events now',
+  FUTURE: 'There are no future events now'
+};
+
+const DateFormat = {
+  DATE:'MMM DD',
+  TIME: 'HH:mm',
+  DURATION: 'DD[D] HH[H] mm[M]',
+  CALENDAR: 'DD/MM/YY HH:mm'
+};
+
+const InfoMessageByAction = {
+  LOADING: 'Loading...',
+  ERROR: 'Failed to load latest route information',
+};
+
+
+const ModeType = {
+  'DEFAULT': 'DEFAULT',
+  'EDIT': 'EDIT',
+  'NEW': 'NEW'
+};
+
+const DEFAULT_SORT_TYPE = SortingType.DAY.name;
+
+const DEFAULT_FILTER_TYPE = FilterType.EVERYTHING;
+
+const UserAction = {
+  UPDATE_EVENT: 'UPDATE_EVENT',
+  ADD_EVENT: 'ADD_EVENT',
+  DELETE_EVENT: 'DELETE_EVENT',
+};
+
+const UpdateType = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+  INIT: 'INIT',
+  ERROR: 'ERROR'
+};
+
+const ApiMethod = {
+  POST: 'POST',
+  GET: 'GET',
+  PUT: 'PUT',
+  DELETE: 'DELETE'
+};
+
+const BlockerTimeLimit = {
+  LOWER_LIMIT: 350,
+  UPPER_LIMIT: 1000,
+};
+
+export {
+  TRIP_TYPES,
+  DateFormat,
+  BLANK_POINT,
+  EmptyListMessage,
+  FilterType,
+  SortingType,
+  DEFAULT_SORT_TYPE,
+  DEFAULT_FILTER_TYPE,
+  InfoMessageByAction,
+  ModeType,
+  UserAction,
+  UpdateType,
+  BASE_URL,
+  AUTHORIZATION_STRING,
+  ApiMethod,
+  BlockerTimeLimit
+};
--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,27 +1,80 @@
 import {POINTS} from '../mock/points.js';
 import {DESTINATIONS} from '../mock/destinations.js';
 import {OFFERS} from '../mock/offers.js';
+import Observable from '../framework/observable.js';
 
-export default class PointsModel {
-  points = POINTS;
-  destinations = DESTINATIONS;
-  offers = OFFERS;
+export default class PointsModel extends Observable {
+  #points;
+  #destinations;
+  #offers;
 
-  getPoints() {
-    return this.points;
+  init() {
+    this.#points = POINTS;
+    this.#offers = OFFERS;
+    this.#destinations = DESTINATIONS;
   }
 
-  getDestinations() {
-    return this.destinations;
+  get points() {
+    return this.#points;
+  }
+
+  set points(routePoints) {
+    this.#points = [...routePoints];
+  }
+
+  get destinations() {
+    return this.#destinations;
+  }
+
+  get offers() {
+    return this.#offers;
+  }
+
+  updatePoint(updateType, updated) {
+    const index = this.#points.findIndex((task) => task.id === updated.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting task');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      updated,
+      ...this.#points.slice(index + 1),
+    ];
+
+    this._notify(updateType, updated);
+  }
+
+  addPoint(updateType, added) {
+    const isPointExists = this.#points.some((point) => point.id === added.id);
+    if (!isPointExists) {
+      this.#points = [...this.#points, added];
+      this._notify(updateType, added);
+    }
+  }
+
+  deletePoint(updateType, point) {
+    const updatedPoints = this.#points.filter((item) => item.id !== point.id);
+    this.#points = updatedPoints;
+    this._notify(updateType);
+  }
+
+  getContentById(id) {
+    const point = this.#points.find((item) => item.id === id);
+    const destination = this.#destinations.find((item) => item.id === point.destination);
+    const offers = this.#offers.find((item) => item.type === point.type.toLocaleLowerCase());
+
+    return {
+      point: point ?? {},
+      destination: destination ?? {},
+      offers: offers.offers ?? {}
+    };
   }
 
   getDestinationByID(id) {
     const allDestinations = this.getDestinations();
     return allDestinations.find((destination) => destination.id === id);
-  }
-
-  getOffers(type) {
-    return this.offers.find((offer) => offer.type === type);
   }
 
   getOffersById(type, offers) {
